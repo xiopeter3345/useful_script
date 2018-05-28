b14687d550501634bd7df27d00da81d4e74d7900
xiopeter3345 <xiopeter3345@outlook.com>
2018-05-28 15:45:00 +0800

    Update change_worker_config.sh
    fix incorrect path to python program

--- StormWorkerConfig/change_worker_config.sh
+++ StormWorkerConfig/change_worker_config.sh
@@ -8,7 +8,7 @@ fi
 mkdir /tmp/changeworkerconfig
 cd /tmp/changeworkerconfig
 
-wget -q -O /tmp/changeworkerconfig/change_worker_config.py https://raw.githubusercontent.com/xiopeter3345/useful_script/master/change_worker_config.py
+wget -q -O /tmp/changeworkerconfig/change_worker_config.py https://raw.githubusercontent.com/xiopeter3345/useful_script/master/StormWorkerConfig/change_worker_config.py
 
 chmod u+x /tmp/changeworkerconfig/change_worker_config.py
 
