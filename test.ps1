1a3099dae635f66fdd4e32c6a5f32399a18ee0bb
xiopeter3345 <xiopeter3345@outlook.com>
2022-05-27 16:56:35 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,9 +3,9 @@ $synapseTokens = @{"`#`#azsynapsewks`#`#" = $SynapseWorkspaceName; }
 $indexFileUrl = "https://raw.githubusercontent.com/Azure/azure-synapse-analytics-end2end/main/Sample/index.json"
 $sampleCodeIndex = Invoke-WebRequest $indexFileUrl | ConvertFrom-Json
 
-Uninstall-Module -Name Az.Synapse -RequiredVersion 1.4.0
+Uninstall-Module -Name Az.Synapse -RequiredVersion 1.4.0 -Force
 
-Install-Module -Name Az.Synapse -RequiredVersion 1.3.0
+Install-Module -Name Az.Synapse -RequiredVersion 1.3.0 -Force
 $accountsVersion = Get-Module -ListAvailable -Name "Az.Accounts"
 Write-Host $accountsVersion.Version
 Get-Module -ListAvailable -Name "Az.Synapse"
