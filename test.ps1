5d6747d6361aa300ef248f9954cc5d7f152ceb60
xiopeter3345 <xiopeter3345@outlook.com>
2022-05-27 17:04:46 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -12,3 +12,28 @@ $accountsVersion = Get-Module -ListAvailable -Name "Az.Accounts"
 Write-Host $accountsVersion.Version
 Get-Module -ListAvailable -Name "Az.Synapse"
 
+foreach($sampleArtifactCollection in $sampleCodeIndex)
+{
+  Write-Host "Deploying Sample Artifact Collection: $($sampleArtifactCollection.template)"
+  Write-Host "-----------------------------------------------------------------------"
+
+  #Create SQL Script artifacts.
+  Write-Host "Deploying SQL Scripts:"
+  Write-Host "-----------------------------------------------------------------------"
+    foreach($sqlScript in $sampleArtifactCollection.artifacts.sqlScripts)
+    {
+      $fileContent = Invoke-WebRequest $sqlScript.definitionFilePath
+
+      if ($sqlScript.tokens.length -gt 0) {
+          foreach($token in $sqlScript.tokens)
+          {
+              $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+          }
+      }
+
+      $definitionFilePath = [guid]::NewGuid()
+      Set-Content -Path $definitionFilePath $fileContent
+      Set-AzSynapseSqlScript -WorkspaceName $SynapseWorkspaceName -Name $sqlScript.name -DefinitionFile $definitionFilePath -FolderPath $sqlScript.workspaceFolderPath
+      Remove-Item -Path $definitionFilePath
+    }
+}
