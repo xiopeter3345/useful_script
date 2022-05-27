bf2189dd43cc399f22d20780a231982653eba855
xiopeter3345 <xiopeter3345@outlook.com>
2022-05-27 17:02:30 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,6 +2,8 @@ $SynapseWorkspaceName = "lazhusynapse"
 $synapseTokens = @{"`#`#azsynapsewks`#`#" = $SynapseWorkspaceName; }
 $indexFileUrl = "https://raw.githubusercontent.com/Azure/azure-synapse-analytics-end2end/main/Sample/index.json"
 $sampleCodeIndex = Invoke-WebRequest $indexFileUrl | ConvertFrom-Json
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
 
 Uninstall-Module -Name Az.Synapse -RequiredVersion 1.4.0 -Force
 
