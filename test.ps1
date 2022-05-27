f41b62baafaaf0339449628c30adfaade7a8d4e0
xiopeter3345 <xiopeter3345@outlook.com>
2022-05-27 16:52:46 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,34 +3,11 @@ $synapseTokens = @{"`#`#azsynapsewks`#`#" = $SynapseWorkspaceName; }
 $indexFileUrl = "https://raw.githubusercontent.com/Azure/azure-synapse-analytics-end2end/main/Sample/index.json"
 $sampleCodeIndex = Invoke-WebRequest $indexFileUrl | ConvertFrom-Json
 
+Uninstall-Module -Name Az.Synapse -RequiredVersion 1.4.0
+
 Install-Module -Name Az.Synapse -RequiredVersion 1.3.0
 $accountsVersion = Get-Module -ListAvailable -Name "Az.Accounts"
 Write-Host $accountsVersion.Version
 $synapseVersion = Get-Module -ListAvailable -Name "Az.Synapse"
 Write-Host $synapseVersion.Version
 
-foreach($sampleArtifactCollection in $sampleCodeIndex)
-{
-  Write-Host "Deploying Sample Artifact Collection: $($sampleArtifactCollection.template)"
-  Write-Host "-----------------------------------------------------------------------"
-
-  #Create SQL Script artifacts.
-  Write-Host "Deploying SQL Scripts:"
-  Write-Host "-----------------------------------------------------------------------"
-    foreach($sqlScript in $sampleArtifactCollection.artifacts.sqlScripts)
-    {
-      $fileContent = Invoke-WebRequest $sqlScript.definitionFilePath
-
-      if ($sqlScript.tokens.length -gt 0) {
-          foreach($token in $sqlScript.tokens)
-          {
-              $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-          }
-      }
-
-      $definitionFilePath = [guid]::NewGuid()
-      Set-Content -Path $definitionFilePath $fileContent
-      Set-AzSynapseSqlScript -WorkspaceName $SynapseWorkspaceName -Name $sqlScript.name -DefinitionFile $definitionFilePath -FolderPath $sqlScript.workspaceFolderPath
-      Remove-Item -Path $definitionFilePath
-    }
-}
